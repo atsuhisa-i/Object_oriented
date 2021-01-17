3986556a7b5cfcd58ee1cca13ad0706be3571630
atsuhisa-i <atsuhisa.1124@gmail.com>
2021-01-17 11:19:21 +0900

    オブジェクトとクラス1

--- Circle.java
+++ Circle.java
@@ -2,17 +2,15 @@ public class Circle
 {
   private int x;
   private int y;
-  private int r;
 
-  public Circle(int x, int y, int r)
+  public Circle(int x, int y)
   {
+    System.out.println("円を作成しました。");
     this.x = x;
     this.y = y;
-    this.r = r;
   }
   public void show()
   {
     System.out.println("円を(" + x + "," + y + ")に表示します。");
-    System.out.println("半径の大きさは" + r + "です。");
   }
 }
