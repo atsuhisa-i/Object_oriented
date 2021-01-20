3f44217c1ab7439400e33db785095e0168f92796
atsuhisa-i <atsuhisa.1124@gmail.com>
2021-01-20 21:27:16 +0900

    関係2

--- Sample2.java
+++ Sample2.java
@@ -1,8 +1,31 @@
-public class Sample2
+import java.util.*;
+import java.awt.*;
+import javax.swing.*;
+
+public class Sample2 extends JFrame
 {
+  private Vector<String> list;
+  private JList ls;
+
   public static void main(String args[])
   {
-    Car c1 = new Car(1234, 10.5);
-    c1.show();
+    Sample2 sm = new Sample2();
+  }
+  public Sample2()
+  {
+    super("サンプル");
+    list = new Vector<String>();
+
+    list.add("Circle");
+    list.add("Rectangle");
+    list.add("Line");
+
+    ls = new JList<String>(list);
+
+    add(ls, BorderLayout.CENTER);
+
+    setDefaultCloseOperation(EXIT_ON_CLOSE);
+    setSize(300, 300);
+    setVisible(true);
   }
 }
