5da2c15533f5b73f71d30e8b4dca318094b59ad7
atsuhisa-i <atsuhisa.1124@gmail.com>
2021-01-20 21:18:42 +0900

    関係1

--- Sample1.java
+++ Sample1.java
@@ -2,7 +2,17 @@ public class Sample1
 {
   public static void main(String args[])
   {
-    Circle c = new Circle(10, 10);
-    c.show();
+    Node n1 = new Node("Line");
+    n1.setNext(null);
+
+    Node n2 = new Node("Rectangle");
+    n2.setNext(n1);
+
+    Node n3 = new Node("Circle");
+    n3.setNext(n2);
+
+    for(Node n = n3; n != null; n = n.getNext()){
+      System.out.println(n);
+    }
   }
 }
