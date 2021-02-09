a9fb92ad5dff4ac711ec5f7b821cb8f53a795590
atsuhisa-i <atsuhisa.1124@gmail.com>
2021-02-09 22:38:45 +0900

    抽象と具象2

--- Application.java
+++ Application.java
@@ -1,5 +1,22 @@
-public interface Application
+import java.io.*;
+import java.util.*;
+
+public abstract class Application
 {
-  public void open(String filename);
-  public void save(String filename);
+  protected ArrayList<String> doc = new ArrayList<String>();
+
+  public void open(String filename)
+  {
+    try{
+      BufferedReader br = new BufferedReader(
+        new FileReader(filename));
+      String line = null;
+      while((line = br.readLine()) != null){
+        doc.add(line);
+      }
+      br.close();
+    }
+    catch(IOException e){}
+  }
+  public abstract void save(String filename);
 }
