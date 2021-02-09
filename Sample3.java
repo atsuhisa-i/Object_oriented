45fdba60544a11c751c950e3c86948fc5082ef33
atsuhisa-i <atsuhisa.1124@gmail.com>
2021-02-09 22:58:15 +0900

    抽象と具象3

--- Sample3.java
+++ Sample3.java
@@ -1,61 +1,12 @@
-import java.util.*;
-import java.text.*;
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
+import java.io.*;
 
-public class Sample3 extends JFrame
+public class Sample3
 {
-  private JList ls;
-
   public static void main(String args[])
   {
-    Sample3 sm = new Sample3();
-  }
-  public Sample3()
-  {
-    super("サンプル");
-
-    Section s1 = new Section("Company");
-    Section s2 = new Section("Trade");
-    Section s3 = new Section("Accounting");
-
-    Person p1 = new Person("Sato");
-    Person p2 = new Person("Takahashi");
-    Person p3 = new Person("Suzuki");
-
-    s1.add(s2);
-    s1.add(s3);
-    s2.add(p1);
-    s2.add(p2);
-    s3.add(p3);
-
-    Visitor v = new NameVisitor();
-
-    Node root = s1;
-
-    walkTree(root, v);
-
-    Vector<String> nl = v.getList();
-
-    ls = new JList<String>(nl);
-
-    add(ls, BorderLayout.CENTER);
-
-    setDefaultCloseOperation(EXIT_ON_CLOSE);
-    setSize(300, 300);
-    setVisible(true);
-  }
-  public void walkTree(Node n, Visitor v)
-  {
-    Vector<Node> children = n.getChildren();
-    if(children == null) return;
-    Iterator<Node> it = children.iterator();
-
-    while(it.hasNext()){
-      Node nn = it.next();
-      nn.accept(v);
-      walkTree(nn, v);
-    }
+    Application ap = new HTMLApplication();
+    ap.open(args[0]);
+    ap.save(args[1]);
+    System.out.println(args[1] + "に出力しました。");
   }
 }
